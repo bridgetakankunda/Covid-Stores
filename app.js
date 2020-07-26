050282ff6ae562a7f16517cef3666eecce3cf1e1
bridgetakankunda <64187124+bridgetakankunda@users.noreply.github.com>
2020-07-26 09:59:43 +0300

    added files and folders

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const express = require('express');
 const app = express();
 
 const nodemon = require('nodemon')
+const mongoose = require('mongoose')
 const pug = require('pug')
 const path = require('path')
 const bodyParser = require('body-parser') 
 const loginRoutes = require('./routes/loginRoutes')
 
+
+// Database connection
+mongoose.connect('mongodb://localhost:27017/Covid_Stores', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, function (err) {
+  if (err) throw err;
+  else
+    console.log('You are live on the database');
+})
+
 // Middleware
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true })) 
 
 app.set('view engine','pug');
 app.set('views', './views');
--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router()
 
-const path = require('path')
+const path = require('path');
+const { read } = require('fs');
 
 // Main login page
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views', '/login.html'))
+})
+
+router.post('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', '/homePage.html'))
 })
 
 //Sales Agent Registration Page
@@ -23,7 +28,12 @@ router.get('/purchaseReg', (req, res) => {
     res.sendFile(path.join(__dirname, '../views', '/purchaseReg.html'))
 })
 
+router.get('/customerReg', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', '/customerReg.html'))
+})
 
-
+router.get('/homePage', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', '/homePage.html'))
+})
 
 module.exports = router;
